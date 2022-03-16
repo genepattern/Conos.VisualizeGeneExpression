99338bd8eef41a436b3bf87cb9b95e6e74f2ecfd
Anthony S. Castanza <acastanza@ucsd.edu>
2022-03-16 15:49:41 -0700

    Fix for Pagoda2 objects using LargeVIS

--- src/run_conos_visualizegeneexpression.R
+++ src/run_conos_visualizegeneexpression.R
@@ -100,8 +100,12 @@ runleiden <- conos_object$runleiden
 runwalktrap <- conos_object$runwalktrap
 data_source <- conos_object$data_source
 
-if (!is.null(con$samples[[1]]@reductions$umap)){
-  embeddings <- 'umap'} else {embeddings <- 'tsne'
+if ("Pagoda2" %in% class(con2$samples[[1]])) {
+  embeddings <- NULL
+} else if (!is.null(con$samples[[1]]@reductions$umap)) {
+ embeddings <- "umap"
+} else {
+ embeddings <- "tsne"
 }
 print('...done!')
 print('Making polots now...')
